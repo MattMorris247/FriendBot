d07f72088fc5560cd475ebba3a93c7b3a1d5d79e
Matt Morris <73907438+MattMorris247@users.noreply.github.com>
2023-02-04 12:54:07 -0500

    Hellowork + colors

---
+++ FriendBotDriver.java
@@ -0,0 +1,21 @@
+
+public class FriendBotDriver {
+
+    /**
+     * Colar Scheme Console
+     * Make sure to place a RESET after any color change...
+     */
+    public static final String RESET = "\033[0m";
+    public static final String BLUE = "\u001B[34m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String PURP = "\u001B[35m";
+    public static final String RED = "\u001B[31m";
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # FriendBot
- Project for State Farm's "Love your neighbor" challenge for UGA HACKS 8
+ Project for State Farm's "Love your neighbor" challenge for UGA HACKS 8 - Molly Gilson and Matt Morris 
